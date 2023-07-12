84b64f118cbb9e7e9c001717c2fd3b89c6bac396
Daniel Kita <t.trax0@gmail.com>
2023-07-12 12:38:14 +0200

    init commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+const server = Bun.serve({
+  port: process.env.PORT || 3456,
+  async fetch(req) {
+    const url = new URL(req.url);
+    // handle POST REQUEST
+    if (req.method === "POST") {
+      // save file to disk
+      try {
+        const file = await req.formData();
+        const data = file.get("file") as Blob;
+        const buffer = Buffer.from(await data.arrayBuffer());
+        const id = crypto.randomUUID();
+
+        await Bun.write(`upload/${id}`, buffer);
+
+        return new Response(`/upload/${id}`);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    if (req.method === "GET") {
+      if (url.pathname === "/") {
+        return new Response(`file uploader`);
+      }
+      const id = url.pathname.split("/").pop();
+      const file = Bun.file(`upload/${id}`);
+      if (!file.size) {
+        return new Response(`File not found`, { status: 404 });
+      }
+      
+      const response = await file.stream();
+
+      setTimeout(() => {
+        // one time download - delete file after 5 seconds
+        Bun.write(`upload/${id}`, "");
+      }, 5000);
+
+      return new Response(response);
+    }
+    return new Response(`hey!`);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}...`);
